711f2832206d689c798f26863a391973c588e9e1
dovekie <dovekie@gmail.com>
2015-06-27 16:27:31 -0700

    javascript for morse lessons

---
+++ js/lessons.js
@@ -0,0 +1,18 @@
+var LESSONS = 
+	{
+		"Lesson1": {
+			soundURL: "whatever",
+			text: "hello world",
+			//morse: "whatever"
+			},
+		"Lesson2": {
+			soundURL: "whatever",
+			text: "attending neohack 15 at rackspace",
+			//morse: "whatever"
+		}
+		"Lesson3": {
+			soundURL: "whatever",
+			text: "order chocolate chip cookies for 17 people",
+			//morse: "whatever"
+		}
+	};
